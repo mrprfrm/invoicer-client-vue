7f2e942b2ce41bf6ae761a221b1f4eb41bdcf089
Anton Petrov <marblemonolit@gmail.com>
2022-07-26 16:08:51 +0300

    Date input behavior, date input tests.

--- src/components/DateInput.vue
+++ src/components/DateInput.vue
@@ -1,25 +1,7 @@
 <script setup>
-import { ref, defineProps, defineEmits, computed } from "vue";
+import { ref, defineProps, defineEmits, computed, watch } from "vue";
 
-const props = defineProps({
-  name: String,
-  label: String,
-  modelValue: String,
-  required: {
-    type: Boolean,
-    default: true,
-  },
-  yearsNumber: {
-    type: Number,
-    default: 30,
-  },
-});
-
-const emit = defineEmits(["update:modelValue"]);
-
-const currentYear = new Date().getFullYear();
-
-// const days = Array.from({ length: 31 }, (_, i) => `0${i + 1}`.slice(-2));
+// Constants
 
 const months = [
   "January",
@@ -36,32 +18,219 @@ const months = [
   "December",
 ];
 
+const SelectionRange = {
+  DAY: 0,
+  MONTH: 1,
+  YEAR: 2,
+};
+
+// Hooks
+
+const props = defineProps({
+  name: String,
+  label: String,
+  modelValue: String,
+  required: {
+    type: Boolean,
+    default: true,
+  },
+  lastYear: {
+    type: Number,
+    default: new Date().getFullYear(),
+  },
+  yearsNumber: {
+    type: Number,
+    default: 30,
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
 // Reactive data
 
 const input = ref(null);
+
 const isBlurable = ref(true);
 const isFocused = ref(false);
+const isReverseFocus = ref(false);
 
-const day = computed({
-  get() {
-    return props.modelValue ? props.modelValue.getDay() : null;
-  },
-  set() {
-    emit("update:modelValue", "1111-11-11");
-  },
+// current selection range
+const csr = ref(null);
+const csrInterval = ref(null);
+
+const day = ref(null);
+const month = ref(null);
+const year = ref(null);
+const pressCount = ref(0);
+
+const value = computed(() => {
+  const d = day.value ? `0${day.value}`.slice(-2) : "__";
+  const m = month.value ? `0${month.value}`.slice(-2) : "__";
+  const y = year.value ? `000${year.value}`.slice(-4) : "____";
+  return `${d}-${m}-${y}`;
 });
 
-const month = computed({
-  get() {
-    return props.modelValue ? props.modelValue.getMonth() : null;
-  },
+// Global events listeners
+
+window.addEventListener("keydown", (evt) => {
+  if (!isFocused.value) {
+    isReverseFocus.value = evt.shiftKey;
+  }
 });
 
-const year = computed({
-  get() {
-    return props.modelValue ? props.modelValue.getFullYear() : null;
-  },
+window.addEventListener("click", () => {
+  isReverseFocus.value = false;
 });
+
+// Watchers
+
+watch(csr, (newCSR, oldCSR) => {
+  if (newCSR !== oldCSR) {
+    pressCount.value = 0;
+  }
+});
+
+// Methods
+function cleanValue() {
+  if (csr.value === SelectionRange.DAY) {
+    day.value = null;
+  } else if (csr.value === SelectionRange.MONTH) {
+    month.value = null;
+  } else if (csr.value === SelectionRange.YEAR) {
+    year.value = null;
+  }
+}
+
+function setValue(evt) {
+  if (evt.keyCode < 47 || evt.keyCode > 57) {
+    return;
+  }
+  pressCount.value++;
+  if (csr.value === SelectionRange.DAY) {
+    if (pressCount.value === 1) {
+      day.value = evt.key;
+    } else {
+      const newDay = day.value + evt.key;
+      day.value = newDay < 31 ? newDay : 31;
+      csr.value = SelectionRange.MONTH;
+    }
+  } else if (csr.value === SelectionRange.MONTH) {
+    if (pressCount.value === 1) {
+      month.value = evt.key;
+    } else {
+      const newMonth = month.value + evt.key;
+      month.value = newMonth < 12 ? newMonth : 12;
+      csr.value = SelectionRange.YEAR;
+    }
+  } else if (csr.value === SelectionRange.YEAR) {
+    if (pressCount.value === 1) {
+      year.value = evt.key;
+    } else {
+      year.value += evt.key;
+    }
+  }
+}
+
+function incrementValue() {
+  if (csr.value === SelectionRange.DAY) {
+    day.value = day.value < 31 ? day.value + 1 : 1;
+  } else if (csr.value === SelectionRange.MONTH) {
+    month.value = month.value < 12 ? month.value + 1 : 1;
+  } else if (csr.value === SelectionRange.YEAR) {
+    year.value++;
+  }
+}
+
+function decrementValue() {
+  if (csr.value === SelectionRange.DAY) {
+    day.value = day.value > 1 ? day.value - 1 : 31;
+  } else if (csr.value === SelectionRange.MONTH) {
+    month.value = month.value > 1 ? month.value - 1 : 12;
+  } else if (csr.value === SelectionRange.YEAR) {
+    year.value = year.value > 1 ? year.value - 1 : props.lastYear;
+  }
+}
+
+function refreshSelection() {
+  // When user click on input, event with preventDefault(),
+  // sometimes browser can allow user to set cursor to specific position.
+  // With current implementation based on interval,
+  // event if user will set cursor in specific position,
+  // this user selection will be changed with current interval function.
+
+  if (csr.value === SelectionRange.DAY) {
+    input.value.setSelectionRange(0, 2);
+  } else if (csr.value === SelectionRange.MONTH) {
+    input.value.setSelectionRange(3, 5);
+  } else if (csr.value === SelectionRange.YEAR) {
+    input.value.setSelectionRange(6, 10);
+  }
+}
+
+function incrementCSR() {
+  if (csr.value < SelectionRange.YEAR) {
+    csr.value++;
+  }
+}
+
+function decrementCSR() {
+  if (csr.value > SelectionRange.DAY) {
+    csr.value--;
+  }
+}
+
+function nextSelectionRange(evt) {
+  if (csr.value != SelectionRange.YEAR) {
+    evt.preventDefault();
+  }
+  csr.value++;
+}
+
+function prevSelectionRange(evt) {
+  if (csr.value != SelectionRange.DAY) {
+    evt.preventDefault();
+  }
+  csr.value--;
+}
+
+function selectDateSection(evt) {
+  // Takes current cursor position on input
+  // and sets appropriate selection range
+
+  if (isFocused.value) {
+    evt.preventDefault();
+  }
+
+  const inputStyle = window.getComputedStyle(input.value, null);
+  const fontSize = parseFloat(inputStyle.getPropertyValue("font-size"));
+  const paddingLeft = parseFloat(inputStyle.getPropertyValue("padding-left"));
+
+  const charSize = fontSize * 0.6;
+  const dayLimit = paddingLeft + charSize * 2;
+  const monthLimit = dayLimit + charSize + charSize * 2;
+  const yearLimit = monthLimit + charSize + charSize * 4;
+
+  const posX = evt.clientX - input.value.getBoundingClientRect().left;
+
+  if (posX > paddingLeft && evt.layerX < dayLimit) {
+    csr.value = SelectionRange.DAY;
+  } else if (posX > dayLimit && evt.layerX < monthLimit) {
+    csr.value = SelectionRange.MONTH;
+  } else if (posX > monthLimit && evt.layerX < yearLimit) {
+    csr.value = SelectionRange.YEAR;
+  }
+}
+
+function focusInput() {
+  input.value.setSelectionRange(0, 0);
+
+  isFocused.value = true;
+  if (!csr.value) {
+    csr.value = isReverseFocus.value ? SelectionRange.YEAR : SelectionRange.DAY;
+  }
+
+  csrInterval.value = setInterval(refreshSelection, 50);
+}
 
 function blurInput() {
   if (!isBlurable.value) {
@@ -69,7 +238,10 @@ function blurInput() {
   }
   // TODO If required validate input has not empty
   emit("update:modelValue", input.value.value);
+  clearInterval(csrInterval);
+  csrInterval.value = null;
   isFocused.value = false;
+  csr.value = null;
 }
 
 function disableInputBlur() {
@@ -90,16 +262,27 @@ function enableInputBlur() {
     >
       {{ label }}
     </label>
+
     <input
       :id="name"
       :name="name"
-      :value="modelValue"
+      :value="value"
       :required="required"
-      @focus="isFocused = true"
+      @keydown.tab.exact="nextSelectionRange"
+      @keydown.shift.tab="prevSelectionRange"
+      @keydown.right.prevent="incrementCSR"
+      @keydown.left.prevent="decrementCSR"
+      @keydown.up.prevent="incrementValue"
+      @keydown.down.prevent="decrementValue"
+      @keydown.backspace.prevent="cleanValue"
+      @mousedown="selectDateSection"
+      @keydown.exact.prevent="setValue"
+      @focus="focusInput"
       @blur="blurInput"
-      type="date"
+      @paste.prevent
+      type="text"
       ref="input"
-      class="text-sm py-3 px-4 border rounded-md focus:ring-brand focus:border-brand"
+      class="text-sm py-3 px-4 border rounded-md focus:ring-brand focus:border-brand cursor-default"
     />
 
     <div
@@ -166,12 +349,12 @@ function enableInputBlur() {
             <button
               v-for="i in yearsNumber"
               :key="name + '-year-' + i"
-              @click="year = currentYear - i + 1"
+              @click="year = lastYear - i + 1"
               type="button"
               tabindex="-1"
               class="block px-4 py-2"
             >
-              {{ currentYear - i + 1 }}
+              {{ lastYear - i + 1 }}
             </button>
           </div>
         </div>
--- tests/unit/dateinput.spec.js
+++ tests/unit/dateinput.spec.js
@@ -5,7 +5,9 @@ import store from "@/store";
 function factory() {
   return shallowMount(DateInput, {
     props: {
-      name: "test",
+      name: "test-date",
+      label: "Test date",
+      modelValue: "",
     },
     globals: {
       plugins: [store],
@@ -13,28 +15,241 @@ function factory() {
   });
 }
 
-describe("Date input tests", () => {
+describe("Date tests", () => {
+  it("Input day", async () => {
+    const wrapper = factory();
+    const input = wrapper.find("input");
+
+    await input.trigger("focus");
+
+    await input.trigger("keydown", { key: "0", keyCode: 48 });
+    expect(input.element.value).toBe("00-__-____");
+
+    await input.trigger("keydown", { key: "1", keyCode: 49 });
+    expect(input.element.value).toBe("01-__-____");
+
+    await input.trigger("keydown.shift.tab");
+
+    await input.trigger("keydown", { key: "1", keyCode: 49 });
+    expect(input.element.value).toBe("01-__-____");
+
+    await input.trigger("keydown", { key: "1", keyCode: 49 });
+    expect(input.element.value).toBe("11-__-____");
+  });
+
+  it("Input month", async () => {
+    const wrapper = factory();
+    const input = wrapper.find("input");
+
+    await input.trigger("focus");
+    await input.trigger("keydown.tab");
+
+    await input.trigger("keydown", { key: "0", keyCode: 48 });
+    expect(input.element.value).toBe("__-00-____");
+
+    await input.trigger("keydown", { key: "1", keyCode: 49 });
+    expect(input.element.value).toBe("__-01-____");
+
+    await input.trigger("keydown.shift.tab");
+
+    await input.trigger("keydown", { key: "1", keyCode: 49 });
+    expect(input.element.value).toBe("__-01-____");
+
+    await input.trigger("keydown", { key: "1", keyCode: 49 });
+    expect(input.element.value).toBe("__-11-____");
+  });
+
+  it("Input year", async () => {
+    const wrapper = factory();
+    const input = wrapper.find("input");
+
+    await input.trigger("focus");
+    await input.trigger("keydown.tab");
+    await input.trigger("keydown.tab");
+
+    await input.trigger("keydown", { key: "0", keyCode: 48 });
+    expect(input.element.value).toBe("__-__-0000");
+
+    await input.trigger("keydown", { key: "1", keyCode: 49 });
+    expect(input.element.value).toBe("__-__-0001");
+
+    await input.trigger("keydown", { key: "0", keyCode: 48 });
+    expect(input.element.value).toBe("__-__-0010");
+
+    await input.trigger("keydown", { key: "1", keyCode: 49 });
+    expect(input.element.value).toBe("__-__-0101");
+
+    await input.trigger("keydown", { key: "0", keyCode: 48 });
+    expect(input.element.value).toBe("__-__-1010");
+  });
+
+  it("Press up day", async () => {
+    const wrapper = factory();
+    const input = wrapper.find("input");
+
+    await input.trigger("focus");
+
+    for (let i = 1; i < 32; i++) {
+      const day = `0${i}`.slice(-2);
+      await input.trigger("keydown.up");
+      expect(input.element.value).toBe(`${day}-__-____`);
+    }
+
+    await input.trigger("keydown.up");
+    expect(input.element.value).toBe("01-__-____");
+  });
+
+  it("Press up month", async () => {
+    const wrapper = factory();
+    const input = wrapper.find("input");
+
+    await input.trigger("focus");
+    await input.trigger("keydown.tab");
+
+    for (let i = 1; i < 13; i++) {
+      const month = `0${i}`.slice(-2);
+      await input.trigger("keydown.up");
+      expect(input.element.value).toBe(`__-${month}-____`);
+    }
+
+    await input.trigger("keydown.up");
+    expect(input.element.value).toBe("__-01-____");
+  });
+
+  it("Press up year", async () => {
+    const wrapper = factory();
+    const input = wrapper.find("input");
+
+    await input.trigger("focus");
+    await input.trigger("keydown.tab");
+    await input.trigger("keydown.tab");
+
+    await input.trigger("keydown.up");
+    expect(input.element.value).toBe("__-__-0001");
+
+    await input.trigger("keydown.up");
+    expect(input.element.value).toBe("__-__-0002");
+  });
+
+  it("Press down day", async () => {
+    const wrapper = factory();
+    const input = wrapper.find("input");
+
+    await input.trigger("focus");
+
+    for (let i = 31; i > 0; i--) {
+      const day = `0${i}`.slice(-2);
+      await input.trigger("keydown.down");
+      expect(input.element.value).toBe(`${day}-__-____`);
+    }
+
+    await input.trigger("keydown.down");
+    expect(input.element.value).toBe("31-__-____");
+  });
+
+  it("Press down month", async () => {
+    const wrapper = factory();
+    const input = wrapper.find("input");
+
+    await input.trigger("focus");
+    await input.trigger("keydown.tab");
+
+    for (let i = 12; i > 0; i--) {
+      const month = `0${i}`.slice(-2);
+      await input.trigger("keydown.down");
+      expect(input.element.value).toBe(`__-${month}-____`);
+    }
+
+    await input.trigger("keydown.down");
+    expect(input.element.value).toBe("__-12-____");
+  });
+
+  it("Press down year", async () => {
+    const wrapper = factory();
+    const input = wrapper.find("input");
+
+    await input.trigger("focus");
+    await input.trigger("keydown.tab");
+    await input.trigger("keydown.tab");
+
+    await input.trigger("keydown.down");
+    expect(input.element.value).toBe("__-__-2022");
+
+    await input.trigger("keydown.down");
+    expect(input.element.value).toBe("__-__-2021");
+  });
+
+  it("Press backspace day", async () => {
+    const wrapper = factory();
+    const input = wrapper.find("input");
+
+    await input.trigger("focus");
+
+    await input.trigger("keydown.up");
+    expect(input.element.value).toBe("01-__-____");
+
+    await input.trigger("keydown.backspace");
+    expect(input.element.value).toBe("__-__-____");
+  });
+
+  it("Press backspace month", async () => {
+    const wrapper = factory();
+    const input = wrapper.find("input");
+
+    await input.trigger("focus");
+    await input.trigger("keydown.tab");
+
+    await input.trigger("keydown.up");
+    expect(input.element.value).toBe("__-01-____");
+
+    await input.trigger("keydown.backspace");
+    expect(input.element.value).toBe("__-__-____");
+  });
+
+  it("Press backspace year", async () => {
+    const wrapper = factory();
+    const input = wrapper.find("input");
+
+    await input.trigger("focus");
+    await input.trigger("keydown.tab");
+    await input.trigger("keydown.tab");
+
+    await input.trigger("keydown.up");
+    expect(input.element.value).toBe("__-__-0001");
+
+    await input.trigger("keydown.backspace");
+    expect(input.element.value).toBe("__-__-____");
+  });
+
+  // TODO backspace tests
+});
+
+describe("Datepicker tests", () => {
   it("Date input focused", async () => {
     const wrapper = factory();
-    await wrapper.find("input").trigger("focus");
-    expect(wrapper.find(".datepicker").exists()).toBe(true);
+    await wrapper.find("input#test-date").trigger("focus");
+    expect(wrapper.find(".datepicker-days").exists()).toBe(true);
+    expect(wrapper.find(".datepicker-months").exists()).toBe(true);
+    expect(wrapper.find(".datepicker-years").exists()).toBe(true);
   });
 
   it("Date input lost focus", async () => {
     const wrapper = factory();
     await wrapper.find("input").trigger("focus");
     await wrapper.find("input").trigger("blur");
-    expect(wrapper.find(".datepicker").exists()).toBe(false);
+    expect(wrapper.find(".datepicker-days").isVisible()).toBe(false);
+    expect(wrapper.find(".datepicker-months").isVisible()).toBe(false);
+    expect(wrapper.find(".datepicker-years").isVisible()).toBe(false);
   });
 
-  it("Date input select date", async () => {
-    const wrapper = factory();
-    await wrapper.find("input").trigger("focus");
-    await wrapper
-      .find(".datepicker")
-      .find(".datepicker__days")
-      .findAll("button")[0]
-      .trigger("click");
-    expect(wrapper.find("input").element.value).toBe("01.mm.yyyy");
-  });
+  // it("Date input select date", async () => {
+  //   const wrapper = factory();
+  //   await wrapper.find("input").trigger("focus");
+  //   // await wrapper
+  //   //   .find(".datepicker")
+  //   //   .find(".datepicker__days")
+  //   //   .findAll("button")[0]
+  //   //   .trigger("click");
+  //   // expect(wrapper.find("input").element.value).toBe("01.mm.yyyy");
+  // });
 });
