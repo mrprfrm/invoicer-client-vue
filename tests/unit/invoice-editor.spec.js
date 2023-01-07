c40da0aac2f24b488162fca2ec6df1e6a0df0950
Anton Petrov <marblemonolit@gmail.com>
2023-01-08 00:24:18 +0400

    Tests naming adjustments, invoice editor tests description, service form tests description.

---
+++ tests/unit/invoice-editor.spec.js
@@ -0,0 +1,13 @@
+describe("Create new invoice", () => {
+  /* TODO: Add tests for required fields in an appropriate tests */
+  /* TODO: Add tests for server side — contract date can not be less than invoice date */
+  it("If all fields are not empty, 'Save' button should be enabled", async () => {});
+  it("If invoice name is empty, 'Save button' should be disabled", async () => {});
+  it("If invoice date is empty, 'Save button' should be disabled", async () => {});
+  it("If contract name is empty, 'Save button' should be disabled", async () => {});
+  it("If contract date is empty, 'Save button' should be disabled", async () => {});
+  it("If invoice has no related contractor, 'Save button' should be disabled", async () => {});
+  it("If invoice has no related client, 'Save button' should be disabled", async () => {});
+  it("If no service was added to invoice, 'Save button' should be disabled", async () => {});
+  it("If terms are empty, 'Save' button should be enabled", async () => {});
+});
---
+++ tests/unit/service-form.spec.js
@@ -0,0 +1,8 @@
+describe("Create new service", () => {
+  it("If all fields are not empty, 'Save' button should be enabled", async () => {});
+  it("If service description is empty, 'Save button' should be disabled", async () => {});
+  it("If service quantity is empty, 'Save button' should be disabled", async () => {});
+  it("If service price is empty, 'Save button' should be disabled", async () => {});
+  /* TODO: add default value for amount equals 1 */
+  it("If service amount is empty, 'Save button' should be disabled", async () => {});
+});
