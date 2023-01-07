c17e305582cea5739616916443a1bdffdb2f1dfb
Anton Petrov <marblemonolit@gmail.com>
2023-01-07 18:44:24 +0400

    Date field modelValue incorporation.

--- src/components/DateField.vue
+++ src/components/DateField.vue
@@ -1,16 +1,42 @@
 <script setup>
-import { reactive, ref, watch } from "vue";
+import { reactive, ref, watch, computed, defineProps, defineEmits } from "vue";
 
 import DatePicker from "@/components/DatePicker.vue";
 import DateInput from "@/components/DateInput.vue";
 
 const field = ref(null);
-const value = ref({ day: 5, month: 2, year: 2020 });
 const state = reactive({
+  fieldValue: null,
   lastNonEmptySection: null,
   currentSection: null,
   blurable: true,
   opened: false,
+});
+
+const props = defineProps(["modelValue"]);
+const emit = defineEmits(["update:modelValue"]);
+
+const value = computed({
+  get() {
+    return props.modelValue
+      ? {
+          day: props.modelValue.getDate(),
+          month: props.modelValue.getMonth() + 1,
+          year: props.modelValue.getFullYear(),
+        }
+      : state.fieldValue || null;
+  },
+  set(value) {
+    if (value.day && value.month && value.year) {
+      emit(
+        "update:modelValue",
+        new Date(value.year, value.month - 1, value.day)
+      );
+      state.fieldValue = null;
+    } else {
+      state.fieldValue = value;
+    }
+  },
 });
 
 watch(
