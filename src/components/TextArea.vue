313ce6e1e7946a84a4349e15579639106e6a260d
Anton Petrov <mrprfrm@gmail.com>
2023-01-25 20:45:47 +0400

    index.html lang added, redundant refs removed, model value added.

--- src/components/TextArea.vue
+++ src/components/TextArea.vue
@@ -1,19 +1,34 @@
 <script setup>
-import { ref, computed, defineProps } from "vue";
+import { computed, defineProps, defineEmits } from "vue";
 
-const props = defineProps(["name", "placeholder", "allowNewLines"]);
+const props = defineProps([
+  "name",
+  "placeholder",
+  "allowNewLines",
+  "modelValue",
+]);
 
-const content = ref(null);
-const text = computed(() =>
-  (content.value || props.placeholder || "").split("\n")
+const emit = defineEmits(["update:modelValue"]);
+
+const text = computed({
+  get() {
+    return props.modelValue || "";
+  },
+  set(value) {
+    emit("update:modelValue", value);
+  },
+});
+
+const content = computed(() =>
+  (props.modelValue || props.placeholder || "").split("\n")
 );
 
-const isNewLinesAllowed = computed(() =>
+const newLinesAllowed = computed(() =>
   [null, undefined, true, "true"].includes(props.allowNewLines)
 );
 
-function enterHandler(evt) {
-  if (!isNewLinesAllowed.value) {
+function onEnter(evt) {
+  if (!newLinesAllowed.value) {
     evt.preventDefault();
   }
 }
@@ -22,9 +37,9 @@ function enterHandler(evt) {
 <template>
   <div class="flex flex-col space-y-1.5 text-base">
     <div class="relative flex flex-col">
-      <div v-if="isNewLinesAllowed" class="flex flex-col">
+      <div v-if="newLinesAllowed" class="flex flex-col">
         <span
-          v-for="(itm, idx) in text"
+          v-for="(itm, idx) in content"
           :key="idx"
           class="whitespace-pre-wrap break-words first:pt-5 last:pb-5 px-4.5 text-transparent"
         >
@@ -33,16 +48,17 @@ function enterHandler(evt) {
       </div>
 
       <span
-        v-if="!isNewLinesAllowed"
-        class="whitespace-pre-wrap break-words first:py-5 px-4.5 text-transparent"
-        >{{ content || placeholder || "NONE" }}</span
+        v-if="!newLinesAllowed"
+        class="whitespace-pre-wrap break-words py-5 px-4.5 text-transparent"
+        >{{ text || placeholder || "NONE" }}</span
       >
       <textarea
         :id="name"
         :name="name"
         :placeholder="placeholder"
-        @input="content = $event.target.value"
-        @keydown.enter="enterHandler"
+        v-model="text"
+        @input="onInput"
+        @keydown.enter="onEnter"
         class="absolute inset-0 z-10 py-5 px-4.5 w-full h-full shadow-inner-violetgray rounded-2.5xl border-none placeholder:text-violetgray-100 focus:ring-1 focus:ring-juicyblue-100"
       />
     </div>
